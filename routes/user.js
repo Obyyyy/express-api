e45b1f1c10873848d6d33d62f476b44afecc1ff9
Obyyyy <qolbymuhammad86@gmail.com>
2023-12-02 21:15:46 +0800

    separate config db

--- routes/user.js
+++ routes/user.js
@@ -7,20 +7,7 @@ const { nanoid } = require("nanoid");
 const mysql = require("mysql");
 const jwt = require("jsonwebtoken");
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "api",
-});
-
-connection.connect((err) => {
-    if (err) {
-        console.error("Error connecting to MySQL:", err);
-        return;
-    }
-    console.log("Connected to MySQL");
-});
+const connection = require("./config/database");
 
 router.post("/register", async (req, res) => {
     try {
