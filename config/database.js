e49f4138c8c77fbdcf6080d753f1369aeb5c400f
Obyyyy <qolbymuhammad86@gmail.com>
2023-12-11 21:42:30 +0800

    add env db

--- config/database.js
+++ config/database.js
@@ -1,12 +1,12 @@
 // db.js
-
+require("dotenv").config();
 const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "api",
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_NAME || "api",
 });
 
 connection.connect((err) => {
